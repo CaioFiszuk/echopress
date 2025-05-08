4bdcb7530973092ec39f529f23cd9ad538251613
Caio Fiszuk <caiolopes871@gmail.com>
2025-05-07 21:28:30 -0300

    Cria a funcionalidade de mostrar as notícias para o usuário comum

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 const newsRoutes = require('./routes/news');
 const usersRoutes = require('./routes/users');
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb://localhost:27017/echopress")
 .then(()=>{
--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from 'react';
+import { api } from '../utils/api';
 
 function App() {
+  const [news, setNews] = useState([]);
+
+  const getAllNews = () => {
+    api.getNews()
+    .then((data)=>{
+      setNews(data);
+      console.log(data);
+    })
+    .catch((error) => console.error("Erro ao buscar as notícias:", error));
+  }
+
+
+  useEffect(()=>{
+    getAllNews();
+  }, []);
 
   return (
     <div>
-
+     {news.map((v)=>(
+      <div key={v._id}>
+         <h1>{v.title}</h1>
+         <img src={v.image} alt="tenso" />
+      </div>
+     ))}
     </div>
   );
 }
